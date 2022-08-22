2feddce688d7e02fa9d9d2dc8e83f31c1642de93
Paramashiva Karanth <86004162+shiva-karanth@users.noreply.github.com>
2022-08-22 19:00:12 +0530

    Create clock_t.c

---
+++ clock_t.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    clock_t start, end;
+    double cpu_time_used;
+    int a=12;
+    int b=11;
+    start = clock();
+    if(!(a&1))
+    {
+        printf("hi");
+    }
+    end = clock();
+    cpu_time_used = ((double) (end - start));
+    printf("%f",cpu_time_used);
+}
+
+
+
+
+// int main()
+// {
+//     clock_t start, end;
+//     double cpu_time_used;
+//     int a=12;
+//     int b=11;
+//     start = clock();
+//     if(a%2==0)
+//     {
+//         printf("hi");
+//     }
+//     end = clock();
+//     cpu_time_used = ((double) (end - start));
+//     printf("%f",cpu_time_used);
+// }
+
