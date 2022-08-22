e3a26bc5341924d47efcb3b9d15ac9b53292b784
Paramashiva Karanth <86004162+shiva-karanth@users.noreply.github.com>
2022-08-22 19:11:38 +0530

    Create equals.java

---
+++ equals.java
@@ -0,0 +1,36 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String s="hi";
+	    String a="hi";                  
+	   
+	   
+	   
+	   //String s=new String("hi");
+	   //String a=new String("hi");       //here we created new address
+	   
+	   
+	    if(a==s)
+	    {
+	        System.out.println("== true");
+	    }
+	    else
+	    {
+	        System.out.println("== false");
+	    }
+	    
+	    
+	    
+	    
+	   if(a.equals(s))
+	    {
+	        System.out.println("== true");
+	    }
+	    else
+	    {
+	        System.out.println("== false");
+	    }
+	}
+}
+
+
