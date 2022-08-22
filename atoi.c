effb4f579073b48dee738f5c9d64b9d45ae4cf5d
Paramashiva Karanth <86004162+shiva-karanth@users.noreply.github.com>
2022-08-22 19:06:53 +0530

    Create atoi.c

---
+++ atoi.c
@@ -0,0 +1,15 @@
+  // https://www.tutorialspoint.com/c_standard_library/index.htm
+#include <stdio.h>
+
+int main()
+{
+    char hi[]="100";
+    char bi[]="200";
+    int x,y;
+    x=atoi(hi);
+    y=atoi(bi);
+    printf("%d",x+y);
+    
+    return 0;
+}
+
