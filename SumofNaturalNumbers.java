150fbe359eefbd9faf0d1cf8da80bdc9e7770f5e
Paramashiva Karanth <86004162+shiva-karanth@users.noreply.github.com>
2022-08-23 23:49:14 +0530

    Create SumofNaturalNumbers.java

---
+++ SumofNaturalNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    System.out.println("enter the number to find sum of natural numbers");
+	    Scanner sc = new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int x=sum(n);
+	    System.out.print(x);
+	    
+	}
+	static int sum(int n)
+	{
+	    if(n==0)
+	    {
+	        return n;
+	    }
+	    else
+	    {
+	        return n+sum(n-1);
+	    }
+	}
+}
