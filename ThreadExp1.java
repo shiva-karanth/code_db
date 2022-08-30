ee395fef62dd87011eb1e43b531a1e5e78efe72b
Paramashiva Karanth <86004162+shiva-karanth@users.noreply.github.com>
2022-08-30 09:35:23 +0530

    Create ThreadExp1.java

---
+++ ThreadExp1.java
@@ -0,0 +1,40 @@
+class hi extends Thread{
+    public void run(){
+        
+        for(int i=0;i<5;i++)
+        {System.out.println("Hi");
+        try{
+        Thread.sleep(500);
+        }
+        catch(Exception e)
+        {
+            
+        }}
+            
+        }
+    
+}
+class hello extends Thread{
+    public void run(){
+        for(int i=0;i<5;i++)
+        {System.out.println("Hello");
+        try{
+        Thread.sleep(200);
+        }
+        catch(Exception e)
+        {
+            
+        }}
+            
+    }
+    
+}
+public class Main
+{
+	public static void main(String[] args) {
+            hi obj1=new hi();
+            hello obj2=new hello();
+            obj1.start();
+            obj2.start();
+	}
+}
